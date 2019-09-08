385e1182227927b8f155577c0f1fd5b3da92ab8c
Mayank Kumar Singh <mayank25031998@gmail.com>
2019-09-08 17:46:29 +0530

    added_upper_threshold

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 seed = 0
 
-dataset_name = 'ICDAR2013_ICDAR2017'
-test_dataset_name = 'ICDAR2013'
+dataset_name = 'ICDAR2015'
+test_dataset_name = 'ICDAR2015'
 
 DataLoaderSYNTH_base_path = '/home/SharedData/Mayank/SynthText/Images'
 DataLoaderSYNTH_mat = '/home/SharedData/Mayank/SynthText/gt.mat'
@@ -15,8 +15,21 @@ target_path = '/home/SharedData/Mayank/'+dataset_name+'/Generated'
 
 Test_Dataset_Path = '/home/SharedData/Mayank/'+test_dataset_name
 
-threshold_character = 0.5
-threshold_affinity = 0.5
+# threshold_character = 0.401288268
+# threshold_affinity = 0.457833362
+
+threshold_character = 0.4
+threshold_affinity = 0.4
+
+# threshold_character_upper = threshold_character + 0.2
+# threshold_affinity_upper = threshold_affinity + 0.2
+
+threshold_character_upper = 0.4
+threshold_affinity_upper = 0.4
+
+scale_character = 25/18.67
+scale_affinity = 25/18.3
+
 threshold_word = 0.7
 threshold_fscore = 0.5
 
